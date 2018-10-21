17faf6379350148a7c79a96f548c3ddfd61ce708
km567 <km567@cornell.edu>
2018-10-21 17:22:50 -0400

    updated authors.ml and authors.mli

---
+++ authors.mli
@@ -0,0 +1,11 @@
+(**
+   Metadata about this submission.
+   @author 
+   Nik Gilevskiy (ng336)
+   Bryan Min (km567)
+   Oscar Barraza (odb5)
+*)
+
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list
