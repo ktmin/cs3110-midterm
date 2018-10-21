53b340bb6fdf7c862780704c75cea1caeccd4d8b
Oscar Barraza <odb5@cornell.edu>
2018-10-21 19:15:14 -0400

    added type t for monopoly

--- monopoly.mli
+++ monopoly.mli
@@ -3,3 +3,9 @@
    This module represents the data stored in monopoly files, including current 
    balance and dice roll. 
 *)
+
+(** The abstract type of values representing adventures. *)
+type t
+
+(** The type of location identifiers. *)
+type location_id = int
