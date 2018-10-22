6ebd84756e9a58c6d813a8ab315544b9c77adca5
Oscar Barraza <odb5@cornell.edu>
2018-10-21 22:39:59 -0400

    added implementation for extracting info from json file

--- monopoly.ml
+++ monopoly.ml
@@ -1,8 +1,29 @@
 open Yojson.Basic.Util
 
 
-type location = {id: int; name: string; description: string; color: string; 
+type location_id = int
+
+type location = {id: location_id; name: string; description: string; color: string; 
                  owned: bool; price: int}
 
-type t = {start_location: int; locations: location list}
+type t = {start_location: location_id; locations: location list}
 
+
+(** [create_locations j] is a record representation of the json information 
+    for each location. *)
+let create_locations j = 
+  {
+    id = j |> member "id" |> to_int;
+    name = j |> member "name" |> to_string;
+    description = j |> member "description" |> to_string;
+    color = j |> member "color" |> to_string;
+    owned = j |> member "owned" |> to_bool;
+    price = j |> member "price" |> to_int
+  }
+
+
+let from_json j = 
+  {
+    start_location = j |> member "start" |> to_int;
+    locations = (j |> member "locations" |> to_list |> List.map create_locations)
+  }
