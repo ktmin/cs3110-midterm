4e4e9c667a16bffac85106676c7274a807f66a76
Oscar Barraza <odb5@cornell.edu>
2018-10-23 23:58:34 -0400

    Added methods to search for a spell, add a spell, and shuffle our spell list (credit needs to be given still)

--- hogwarts.mli
+++ hogwarts.mli
@@ -7,15 +7,37 @@
 (** The abstract type of values representing hogwarts. *)
 type t
 
-type spell_name
 
-type spell
+(** The type of a spell name *)
+type spell_name = string
 
-type spls
+(** The type of a spell's damage *)
+type spell_damage = int 
+
+(** The type of target *)
+type target = string
+
+(** The type of description *)
+type description = string
+
+(** The type of spell  *)
+type spell = {
+  name: spell_name;
+  damage: spell_damage;
+  target: target;
+  description: description;
+}
+
+(** Raised when an unknown spell is encountered *)
+exception UnknownSpell of spell
 
 (** [from_json j] is the organized spells that [j] represents.
     Requires: [j] is a valid JSON spell representation. *)
 val from_json : Yojson.Basic.json -> t
 
-val create_spell : Yojson.Basic.json -> spell
+(** [search spells spell] looks for the [spell] and returns it.
+    Raises: UnknownSpell if [spell] is not found in [spells]  *)
+val search : spell list -> spell_name -> spell
+
+(**  *)
 
