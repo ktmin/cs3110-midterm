f24182cc451cc7b0d228e1b3107546e3cb1e8211
km567 <km567@cornell.edu>
2018-10-23 23:29:41 -0400

    updated hogwarts.mli

--- hogwarts.mli
+++ hogwarts.mli
@@ -5,17 +5,18 @@
 *)
 
 (** The abstract type of values representing hogwarts. *)
-type t
-
 type spell_name
+
+type spell_damage
+
+type target
+
+type description 
 
 type spell
 
-type spls
-
 (** [from_json j] is the organized spells that [j] represents.
     Requires: [j] is a valid JSON spell representation. *)
-val from_json : Yojson.Basic.json -> t
 
 val create_spell : Yojson.Basic.json -> spell
 
