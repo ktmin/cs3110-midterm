f7f0327268f9498c1edc975688dc67d7b3fe3eb4
Oscar Barraza <odb5@cornell.edu>
2018-10-24 09:41:50 -0400

    changed from module to just functions and added the mli

--- state.ml
+++ state.ml
@@ -1,76 +1,122 @@
 open Yojson.Basic.Util
 open Hogwarts
 
-module type Command = sig
-  type player_name
+type player_name = string
+type player_hp = int
 
-  type player_hp
 
-  type player
+type player = {
+  name : player_name;
+  hp: player_hp;
+}
 
-  type hand
+type hand = {
+  hand : spell_info list;
+}
 
-  type deck
+type deck = {
+  deck : spell_info list;
+}
 
-  val get_name: player -> player_name
+let get_name st =
+  st.name
 
-  val get_hp: player -> player_hp 
+let get_hp st = 
+  st.hp 
 
-  val get_hand: hand -> Hogwarts.spell_info list
+let get_hand st =
+  st.hand
 
-  val draw: Hogwarts.spell_info list ->
+(** update hand and deck
+    returns a tuple 
+    of updated hand and 
+    deck*)
+let draw chosen st1 st2=
+  (st1.hand @ chosen, 
+   match st2.deck with
+   | [] -> []
+   | h :: t -> t  
+  ) 
+
+let cast damage chosen st =    
+  (damage, List.filter (fun x -> x <> chosen) st.hand)
+
+(** returns hp after the spell is casted*)
+let casted damage st = 
+  st.hp - damage  
+
+
+
+(* module type Command = sig
+   type player_name
+
+   type player_hp
+
+   type player
+
+   type hand
+
+   type deck
+
+   val get_name: player -> player_name
+
+   val get_hp: player -> player_hp 
+
+   val get_hand: hand -> Hogwarts.spell_info list
+
+   val draw: Hogwarts.spell_info list ->
     hand -> deck -> Hogwarts.spell_info list * Hogwarts.spell_info list
 
-  val cast : 'a -> Hogwarts.spell_info -> hand -> 'a * Hogwarts.spell_info list
+   val cast : 'a -> Hogwarts.spell_info -> hand -> 'a * Hogwarts.spell_info list
 
-end 
+   end 
 
-module Command: Command = struct  
-  type player_name = string
-  type player_hp = int
+   module Command: Command = struct  
+   type player_name = string
+   type player_hp = int
 
 
-  type player = {
+   type player = {
     name : player_name;
     hp: player_hp;
-  }
+   }
 
-  type hand = {
+   type hand = {
     hand : spell_info list;
-  }
+   }
 
-  type deck = {
+   type deck = {
     deck : spell_info list;
-  }
+   }
 
-  let get_name st =
+   let get_name st =
     st.name
 
-  let get_hp st = 
+   let get_hp st = 
     st.hp 
 
-  let get_hand st =
+   let get_hand st =
     st.hand
 
-  (** update hand and deck
+   (** update hand and deck
       returns a tuple 
       of updated hand and 
       deck*)
-  let draw chosen st1 st2=
+   let draw chosen st1 st2=
     (st1.hand @ chosen, 
      match st2.deck with
      | [] -> []
      | h :: t -> t  
     ) 
 
-  let cast damage chosen st =    
+   let cast damage chosen st =    
     (damage,List.filter (fun x -> x <> chosen) st.hand)
 
-  (** returns hp after the spell is casted*)
-  let casted damage st = 
+   (** returns hp after the spell is casted*)
+   let casted damage st = 
     st.hp - damage  
 
-end 
+   end  *)
 
 
 
--- state.mli
+++ state.mli
@@ -4,3 +4,25 @@
    including the player's current positon, current balance, and functions that 
    cause the state to change.
 *)
+
+type player_name = string
+
+type player_hp = int
+
+type player
+
+type hand
+
+type deck
+
+val get_name: player -> player_name
+
+val get_hp: player -> player_hp 
+
+val get_hand: hand -> Hogwarts.spell_info list
+
+val draw: Hogwarts.spell_info list ->
+  hand -> deck -> Hogwarts.spell_info list * Hogwarts.spell_info list
+
+val cast : Hogwarts.damage -> Hogwarts.spell_info -> hand -> 
+  Hogwarts.damage * Hogwarts.spell_info list
