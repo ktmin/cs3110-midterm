b59e11ce067a8db2ae80a07d330fbeb27f7d85c1
km567 <km567@cornell.edu>
2018-10-31 18:37:32 -0400

    added docs for command

--- command.mli
+++ command.mli
@@ -14,4 +14,7 @@ type command =
 
 exception Invalidcommand
 
+
+(**[parse string] prases a string and outputs its corresponding
+   command. Outputs an exception Invalidcommand if a command is malfored*)
 val parse: string -> command
