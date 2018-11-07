4978465fd0223b1c8d066998fde00e440d5afaa7
Oscar Barraza <odb5@cornell.edu>
2018-11-06 19:18:12 -0500

    completed left side of ai decision 'tree'. Also renamed file to 'ai_controller'

--- ai_state.mli
+++
@@ -1,6 +0,0 @@
-(** 
-   Representation of dynamic monopoly state.
-   This module represents the state of a monopoly game for the ai as it is being 
-   played, including the ai's current positon, current balance, and functions that 
-   cause the state to change.
-*)
