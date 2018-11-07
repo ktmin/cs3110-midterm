9046eb26ec2093e3ff761434d7b02cf624012c74
Oscar Barraza <odb5@cornell.edu>
2018-11-06 23:40:08 -0500

    added print statements

--- ai_controller.mli
+++ ai_controller.mli
@@ -7,6 +7,6 @@ module type AI = sig
   val enemy_decision: State.t -> State.t -> State.t * State.t
 end
 
-(** [MakeGame V] uses [View.View] as a printer in a game scenario and contains
-    all game logic to be used. *)
+(** [MakeAI V] uses [View.View] as a printer in a game scenario and contains
+    all ai logic to be used. *)
 module MakeAI (V:Mainview) : AI
